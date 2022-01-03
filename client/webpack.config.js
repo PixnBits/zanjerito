877320885479d9782c013657218041af082aeb13
N Oliver <github@pixnbits.org>
2022-01-02 22:10:35 -0700

    chore(build:client): enable production or development mode switching

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,10 +3,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+const isDev = process.env.NODE_ENV === 'development';
+
 module.exports = {
+  mode: isDev ? 'development' : 'production',
   context: path.resolve(__dirname),
   entry: './index.tsx',
-  devtool: 'inline-source-map',
+  // devtool: 'inline-source-map',
   module: {
     rules: [
       {
