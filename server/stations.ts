bef7b59d300595548043cdc31635f307f68ee69f
N Oliver <github@pixnbits.org>
2022-01-02 23:35:13 -0700

    fix(server): pin number conversion

--- server/stations.ts
+++ server/stations.ts
@@ -21,29 +21,52 @@ export interface StationList extends Array<Station> {
 
 const stationsConfiguration = [
   {
-    channel: 21,
+    // wiringPi 21, physical 29
+    channel: 29,
     title: 'Front West',
-    notes: 'red',
+    notes: 'red, wiringPi 21, physical 29',
   },
   {
-    channel: 22,
+    // wiringPi 22, physical 31
+    channel: 31,
     title: 'Front North',
-    notes: 'yellow',
+    notes: 'yellow, wiringPi 22, physical 31',
   },
   {
-    channel: 23,
+    // wiringPi 23, physical 33
+    channel: 33,
     title: 'Drip Line',
-    notes: 'blue',
+    notes: 'blue, wiringPi 23, physical 33',
   },
   {
-    channel: 24,
+    // wiringPi 24, physical 35
+    channel: 35,
     title: 'Front South',
-    notes: 'green',
+    notes: 'green, wiringPi 24, physical 35',
+  },
+  {
+    // wiringPi 25, physical 37
+    channel: 37,
+    title: '25, physical 37',
+    notes: 'unconnected',
+  },
+  {
+    // wiringPi 27, physical 36
+    channel: 36,
+    title: 'wiringPi 27, physical 36',
+    notes: 'unconnected',
+  },
+  {
+    // wiringPi 28, physical 38
+    channel: 38,
+    title: 'wiringPi 28, physical 38',
+    notes: 'unconnected',
   },
 ];
 
 const powerEnableConfiguration = {
-  channel: 29,
+  // wiringPi 29, physical 40
+  channel: 40,
   title: 'Station PSU',
   notes: '24VAC',
 };
@@ -54,16 +77,16 @@ const IS_DEV = process.env.NODE_ENV === 'development' ? true : false;
 export default async function setup() {
   // using gpio on a non-Raspberry Pi device fails, making development difficult
   if (!IS_DEV) {
+    // MODE_RPI is the default and is the physical pin number
+    // note that the `gpio` CLI tool instead uses wiringPi by default
+    // use `gpio readall` to see the differences
     await Promise.all([
-      // power enable
-      gpio.setup(powerEnableConfiguration.channel, gpio.DIR_OUT)
-        // disable power ASAP
-        .then(() => {
-          gpio.write(powerEnableConfiguration.channel, false)
-        }),
+      // power enable (DIR_OUT) but also off (DIR_HIGH as the relay board inverts)
+      // disabling power ASAP
+      gpio.setup(powerEnableConfiguration.channel, gpio.DIR_HIGH),
       // all the stations
       ...stationsConfiguration.map(
-        ({ channel }) => gpio.setup(channel, gpio.DIR_OUT)
+        ({ channel }) => gpio.setup(channel, gpio.DIR_HIGH)
       ),
     ]);
   }
@@ -75,9 +98,9 @@ export default async function setup() {
       notes: stationConfiguration.notes,
       // currentlyOn: undefined,
       on: async () => {
-        console.log(`GPIO ${channel} true`);
+        console.log(`GPIO ${channel} on (false)`);
         if (!IS_DEV) {
-          gpio.write(channel, true);
+          gpio.write(channel, false);
         }
         subscriptionEmitter.emit({
           topic: STATION_TOGGLED_TOPIC,
@@ -91,9 +114,9 @@ export default async function setup() {
         });
       },
       off: async () => {
-        console.log(`GPIO ${channel} false`);
+        console.log(`GPIO ${channel} off (true)`);
         if (!IS_DEV) {
-          gpio.write(channel, false);
+          gpio.write(channel, true);
         }
         subscriptionEmitter.emit({
           topic: STATION_TOGGLED_TOPIC,
@@ -172,7 +195,7 @@ export default async function setup() {
   );
 
   // need to write functional tests instead, but until then
-  if (process.env.NODE_ENV === 'development') {
+  if (IS_DEV) {
     [
       // [0, 4.5e3],
       [1, 10e3],
@@ -183,6 +206,7 @@ export default async function setup() {
         toggle = !toggle;
         stations[index][toggle ? 'on' : 'off']();
       }, interval);
+      console.log(`set up ${index} to toggle every ${interval}ms`);
     });
   }
 
