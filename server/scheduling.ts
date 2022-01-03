fca74b6661291c5e696d2580aa0ecc74f504e225
N Oliver <github@pixnbits.org>
2022-01-03 00:54:55 -0700

    feat(server): planning, overlap to avoid waterhammer

--- server/scheduling.ts
+++ server/scheduling.ts
@@ -8,6 +8,10 @@ import { addToSchema } from './schema';
 
 const sleep = require('util').promisify(setTimeout);
 
+// avoid water hammer by opening the next valve before closing the previous one
+// disabled for any durations less than this overlap
+const OVERLAP = 2e3;
+
 const schedulesConfiguration = [
   {
     title: 'Seedlings',
@@ -51,16 +55,84 @@ export default async function setup(stations: StationList, power: Station) {
   const schedules = schedulesConfiguration.map(scheduleConfiguration => {
     const jobs = scheduleConfiguration.starts.map(
       rule => scheduler.scheduleJob(rule, async () => {
-        console.info(`running schedule ${scheduleConfiguration.title} (${new Date()})`);
-        await power.on();
+        console.info(`time to run schedule ${scheduleConfiguration.title} (${new Date()})`);
+        const plan = [];
+        let wasCurrentNowPreviousStation = null;
         for await (const { index, duration } of scheduleConfiguration.itinerary) {
-          const parsedDuration = durationFns.parse(duration);
-          console.info(`running station ${index} for ${duration}`, parsedDuration, new Date());
-          stations[index].on();
-          await sleep(durationFns.toMilliseconds(parsedDuration));
-          stations[index].off();
+          const durationMS = durationFns.toMilliseconds(durationFns.parse(duration));
+          const currentStation = stations[index];
+          const previousStation = wasCurrentNowPreviousStation;
+          if (!previousStation) {
+            plan.push({
+              action: () => {
+                console.info(`running station ${index} for ${duration}`, durationMS, new Date());
+                return currentStation.on();
+              },
+              wait: durationMS,
+            });
+          } else {
+            let firstWait = OVERLAP;
+            let secondWait = durationMS - OVERLAP;
+            if (secondWait < 0) {
+              // duration is less than the overlap
+              firstWait = durationMS;
+              secondWait = 0;
+            }
+            plan.push(
+              {
+                action: () => {
+                  console.info(`running station ${index} for ${duration}`, firstWait + secondWait, new Date());
+                  return currentStation.on();
+                },
+                wait: firstWait,
+              },
+              {
+                action: () => {
+                  console.info(`turning off station ${stations.indexOf(previousStation)}`, firstWait, secondWait, new Date());
+                  return previousStation.off();
+                },
+                wait: secondWait,
+              }
+            );
+          }
+
+          wasCurrentNowPreviousStation = currentStation;
         }
-        await power.off();
+
+        if (wasCurrentNowPreviousStation) {
+          const currentStation = wasCurrentNowPreviousStation;
+          plan.push({
+            action: () => {
+              console.info(`turning off station ${stations.indexOf(currentStation)}`, 0, new Date());
+              return currentStation.off();
+            },
+            wait: 0,
+          });
+        }
+
+        if (plan.length > 0) {
+          const startingAction = plan[0].action;
+          plan[0].action = async () => {
+            console.info(`turning on power`, new Date());
+            await power.on();
+            return await startingAction();
+          }
+          const finishingAction = plan[plan.length - 1].action;
+          plan[plan.length - 1].action = async () => {
+            const returnValue = await finishingAction();
+            console.info(`turning off power`, new Date());
+            await power.off();
+            return returnValue;
+          }
+        }
+        console.log('plan', plan);
+        console.info(`running schedule ${scheduleConfiguration.title} (${new Date()})`);
+        for await (const { action, wait } of plan) {
+          await action();
+          if (wait > 0) {
+            await sleep(wait);
+          }
+        }
         console.info(`finished schedule ${scheduleConfiguration.title} (${new Date()})`);
       })
     );
